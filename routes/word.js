5b165280ec892462ca4e799c1e8e2a5422e622cb
Damiant94 <damian.tulacz@gmail.com>
2021-02-03 18:00:44 +0100

    added endpoint for any number of random words lower or equal 1000, added endpoint for random word which starts with queried string

--- routes/word.js
+++ routes/word.js
@@ -1,25 +1,51 @@
 const express = require('express');
-const router = express.Router();
-const Word = require('../models/word');
 const cors = require('cors');
+const router = express.Router();
 router.use(cors());
+
+const Word = require('../models/word');
 
 router.get('/word', async (req, res) => {
     try {
-        const word = await Word.aggregate([{$sample: {size: 1}}]);
-        res.json(word[0].word);
+        if (req.query.firstLetter) {
+            const words = await Word.find({
+                word: new RegExp('^' + req.query.firstLetter, 'i')
+            });
+            if (words.length > 0) {
+                const randomIndex = Math.floor(Math.random() * words.length);
+                const word = words[randomIndex];
+                res.json([word.word]);
+            }
+            else res.json([]);
+        } else {
+            const wordObject = await Word.aggregate([{ 
+                $sample: { size: 1 } 
+            }]);
+            res.json([wordObject[0].word]);
+        }
+
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
 });
 
-// router.get('/', async (req, res) => {
-//     try {
-//         const word = await Word.aggregate([{$sample: {size: 1}}]);
-//         res.json(word[0].word);
-//     } catch (err) {
-//         res.status(500).json({ message: err.message });
-//     }
-// });
+router.get('/words', async (req, res) => {
+    try {
+        if (req.query.number <= 1000) {
+            const words = await Word.aggregate([{
+                $sample: { size: parseInt(req.query.number) }
+            }]);
+            const wordsMapped = words.map((wordObject) => {
+                return wordObject.word;
+            });
+            res.json(wordsMapped);
+        } else {
+            res.json({ message: "query number too high" });
+        }
+
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
 
 module.exports = router;
