3c6ea8fbe06882e38d80a64697c5db5c56d58169
Damian Tulacz <damian.tulacz@gmail.com>
2024-01-08 22:51:54 +0100

    created api folder

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
-        res.render('../views/ejs/index.ejs');
+        res.render('../api/index.ejs');
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
