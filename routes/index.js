bad805a82a772b7bc6e01aac8cb16b8b287bcdc1
Damian Tulacz <damian.tulacz@gmail.com>
2024-01-08 23:21:07 +0100

    changes made

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
-        res.render('../api/index.ejs');
+        res.render('../pages/index.ejs');
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
